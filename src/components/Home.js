40e13028f7692d811211708fb9ab49bf3eec47be
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-11 23:02:46 +0300

    update tablo yerleştirildi

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,36 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 import Access from "./Access"
+import Table from 'react-bootstrap/Table'
 
 const Home = (() => {
+    const [tableHead, setTableHead] = useState([])
     const [projectList, setProjectList] = useState([])
-
 
 
     useEffect(() => {
 
-        axios.post(`${Access.adress}/Project/ProjectList`)
+        axios.post(`${Access}/Project/ProjectList`)
             .then(function ({ data }) {
+                if (data.IsSuccess !== 1) {
+                    const i = []
+                    i.push(<p>Listeye ulaşılmıyor</p>)
+                    setProjectList(i)
+                }
+                else {
+                    const tableHeader = []
+                    const tableBody = []
 
+                    Object.keys(data.Result[0]).forEach((element, i) => tableHeader.push(<th key={i}>{element}</th>))
+                    setTableHead(tableHeader)
+
+                    data.Result.forEach((element, i) => {
+                        const td = []
+                        Object.values(element).forEach((e, i) => td.push(<td key={i}>{e}</td>))
+                        tableBody.push(<tr key={i}>{td}</tr>)
+                    })
+                    setProjectList(tableBody)
+                }
 
 
 
@@ -22,7 +41,17 @@ const Home = (() => {
             })
     }, [])
     return (
-        <p>asd</p>
+        <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                    {tableHead}
+                </tr>
+            </thead>
+            <tbody>
+                {projectList}
+            </tbody>
+        </Table>
+
     )
 })
 
