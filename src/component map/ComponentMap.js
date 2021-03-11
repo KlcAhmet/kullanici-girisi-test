2d4aa9b627d23a81d634fe7ecc5f9498cfa7e578
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-11 23:28:50 +0300

    import Login component

--- src/component map/ComponentMap.js
+++ src/component map/ComponentMap.js
@@ -1,4 +1,5 @@
 /* Pages */
 export { default as Home } from "../components/Home"
+export { default as Login } from "../components/Login"
 /* Components */
 export { default as Navigation } from "../components/Navigation"
