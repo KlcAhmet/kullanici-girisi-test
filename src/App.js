425da9b8a34c146e97530c5ccad81957545728ca
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-11 19:55:24 +0300

    update Router import edildi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
