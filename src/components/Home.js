e680268df8414d2474f3410c2de38bcae6b1179d
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-11 21:15:08 +0300

    update temel içerik yazıldı

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,2 +1,30 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import Access from "./Access"
+
+const Home = (() => {
+    const [projectList, setProjectList] = useState([])
 
 
+
+    useEffect(() => {
+
+        axios.post(`${Access.adress}/Project/ProjectList`)
+            .then(function ({ data }) {
+
+
+
+
+            })
+            .catch(function (error) {
+                console.log("Hata")
+                console.log(error)
+            })
+    }, [])
+    return (
+        <p>asd</p>
+    )
+})
+
+
+export default Home
