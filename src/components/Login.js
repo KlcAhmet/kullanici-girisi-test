811873f1888162b4c19c63ea653750836c1384b5
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-12 03:11:04 +0300

    update içerik yazıldı

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,52 @@
-function Login() {
+import axios from "axios"
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import Access from "./Access"
+
+const Login = e => {
+    const onSubmit = event => {
+        event.preventDefault();
+        var formdata = new FormData()
+        formdata.append('Email', event.target[0].value)
+        formdata.append('Password', event.target[1].value)
+
+        axios({
+            method: "post",
+            url: `${Access}/Account/Login`,
+            data: formdata,
+            headers: { "Content-Type": "multipart/form-data" }
+        })
+            .then(function (data) {
+                //handle success
+                if (data.data.IsSuccess) {
+                    console.log(data)
+
+
+                }
+                else {
+                    alert("Mail yada şifre yanlış!")
+                }
+
+            })
+            .catch(function (error) {
+                console.log("Hata")
+                console.log(error)
+            })
+    }
+
     return (
-        <p>sdfsdf</p>
+        <Form onSubmit={onSubmit}>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">Gönder</Button>
+        </Form>
     )
 }
 
