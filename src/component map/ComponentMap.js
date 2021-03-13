b7d6bdcf4469a00d26665f8a196b7091a3519ef8
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-13 23:48:42 +0300

    update importlar eklendi

--- src/component map/ComponentMap.js
+++ src/component map/ComponentMap.js
@@ -3,3 +3,6 @@ export { default as Home } from "../components/Home"
 export { default as Login } from "../components/Login"
 /* Components */
 export { default as Navigation } from "../components/Navigation"
+export { default as GuestLoggin } from "../components/GuestLoggin"
+export { default as UserLoggin } from "../components/UserLoggin"
+export { default as ProtectedRoute } from "../components/ProtectedRoute"
