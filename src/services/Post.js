7569b741a5bceb45c57262ab145cccbd85c23168
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-29 06:19:57 +0300

    create Post.js

---
+++ src/services/Post.js
@@ -0,0 +1,37 @@
+import axios from "axios"
+import './axiosInterceptors'
+import { Message } from '../component map/ComponentMap'
+import Access from "../components/Access"
+
+// eslint-disable-next-line
+export default {
+    postLoginData(event) {
+
+        const axiosParameters = {
+            headers: { "Content-Type": "multipart/form-data" }
+        }
+
+        var formdata = new FormData()
+        formdata.append('Email', event.target[0].value)
+        formdata.append('Password', event.target[1].value)
+        Message("loading")
+        return axios.post(`${Access}/Account/Login`, formdata, axiosParameters)
+    },
+    postContactList(event) {
+        const axiosParameters = {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                authorization: `Bearer ${Object.values(JSON.parse(localStorage.getItem('Token')))}`
+            }
+        }
+
+        var formdata = new FormData()
+        formdata.append('FullName', event.target[0].value)
+        formdata.append('Email', event.target[1].value)
+        formdata.append('Subject', event.target[2].value)
+        formdata.append('Message', event.target[3].value)
+
+        return axios.post(`${Access}/ContactUs/List`, formdata, axiosParameters)
+    }
+
+}
