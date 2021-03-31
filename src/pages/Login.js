0efb524f19987c3333a4d2a31fb0e78fab3a03e4
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-03-31 04:21:07 +0300

    test içerik kaldırıldı

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,11 +6,16 @@ import store, { login } from "../store/index"
 /* Utils */
 import history from '../utils/history'
 /* Components */
-import { Row, Col } from 'react-bootstrap'
-import { Button, FormGroup, InputGroup, TextArea } from "@blueprintjs/core";
+import { Row, Col, Container } from 'react-bootstrap'
+import { Button, FormGroup, InputGroup } from "@blueprintjs/core";
 
 const Login = props => {
     const Token = useSelector(state => state.Token);
+
+    const goRegister = () => {
+        history.push("register")
+    }
+
     useEffect(() => {
         if (store.getState().Token) {
             saveState({
@@ -24,67 +29,28 @@ const Login = props => {
     }, [Token])
 
     return (
-        <Row className="justify-content-center" style={{ margin: 0 }}>
-            <Col xs={2} >
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    store.dispatch(login(e))
-                }}>
-                    <FormGroup label="Email" labelFor="text-email" labelInfo="(Zorunlu)">
-                        <InputGroup type="email" id="text-email" placeholder="deneme@deneme.com" defaultValue="semihcetin34@gmail.com" />
-                    </FormGroup>
-                    <FormGroup
-                        label="Şifre" labelFor="text-password" labelInfo="(Zorunlu)">
-                        <InputGroup type="password" id="text-password" placeholder="*******" defaultValue="1" />
-                    </FormGroup>
-                    <div className="buttons">
-                        <Button className="buttons-btn bp3-button bp3-intent-success bp3-large" type="submit">Giriş</Button>
-                        <Button className="buttons-btn bp3-button bp3-intent-primary bp3-large" type="button">Kaydol</Button>
-                    </div>
-                </form>
-            </Col>
-            <Col xs={2} >
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    store.dispatch(login(e))
-                }}>
-                    <FormGroup label="Email" labelFor="text-emailadd" labelInfo="(Zorunlu)">
-                        <InputGroup type="email" id="text-emailadd" placeholder="deneme@serd.com" palaceholder="deneme@deneme.com" />
-                    </FormGroup>
-                    <FormGroup
-                        label="Şifre" labelFor="text-passwordadd" labelInfo="(Zorunlu)">
-                        <InputGroup type="password" id="text-passwordadd" placeholder="*******" />
-                    </FormGroup>
-                    <FormGroup
-                        label="İsim" labelFor="text-firstName" labelInfo="(Zorunlu)">
-                        <InputGroup type="text" id="text-Name" />
-                    </FormGroup>
-                    <FormGroup
-                        label="Soyisim" labelFor="text-lastName" labelInfo="(Zorunlu)">
-                        <InputGroup type="text" id="text-lastName" />
-                    </FormGroup>
-                    <FormGroup
-                        label="Telefon Numarası" labelFor="text-phone" labelInfo="(Zorunlu)">
-                        <InputGroup type="text" id="text-phone" />
-                    </FormGroup>
-                    <FormGroup
-                        label="Doğum Günü" labelFor="text-birthDate" labelInfo="(Zorunlu)">
-                        <InputGroup type="date" id="text-birthDate" />
-                    </FormGroup>
-                    <FormGroup
-                        label="Cinsiyet" labelFor="text-gender" labelInfo="(Zorunlu)">
-                        <InputGroup type="text" id="text-gender" />
-                    </FormGroup>
-                    <FormGroup
-                        label="Adres" labelFor="text-address" labelInfo="(Zorunlu)">
-                        <TextArea fill={true} />
-                    </FormGroup>
-                    <div className="buttons">
-                        <Button className="buttons-btn bp3-button bp3-intent-primary bp3-large" type="button">Kaydol</Button>
-                    </div>
-                </form>
-            </Col>
-        </Row >
+        <Container>
+            <Row className="justify-content-center">
+                <Col xs={3} >
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        store.dispatch(login(e))
+                    }}>
+                        <FormGroup label="Email" labelFor="text-email" labelInfo="(Zorunlu)">
+                            <InputGroup type="email" id="text-email" placeholder="deneme@deneme.com" defaultValue="semihcetin34@gmail.com" />
+                        </FormGroup>
+                        <FormGroup
+                            label="Şifre" labelFor="text-password" labelInfo="(Zorunlu)">
+                            <InputGroup type="password" id="text-password" placeholder="*******" defaultValue="1" />
+                        </FormGroup>
+                        <div className="buttons">
+                            <Button className="buttons-btn bp3-button bp3-intent-success bp3-large" type="submit">Giriş</Button>
+                            <Button className="buttons-btn bp3-button bp3-intent-primary bp3-large" type="button" onClick={goRegister}>Kaydol</Button>
+                        </div>
+                    </form>
+                </Col>
+            </Row >
+        </Container>
     )
 }
 
