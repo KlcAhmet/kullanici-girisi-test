1e4e443b18b239ecd534abc2a788fbd18d679174
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-02 21:45:48 +0300

    update ForgotPassword.js

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -5,20 +5,20 @@ import Post from '../services/Post'
 /* Components */
 import { Row, Col, Container } from 'react-bootstrap'
 import { Button, FormGroup, InputGroup } from "@blueprintjs/core";
+/* Css */
+import '../css/ForgotPassword.css'
 
 const ForgotPassword = () => {
     return (
         <Container>
-            <Row className="justify-content-center">
-                <Col xs={4} >
-                    <div className="forgot">
-                        <h3 className="forgot-title">Şifre yenileme</h3>
-                        <p className="forgot-subtitle">Şifre yenileme bağlantısını gönderebilmemiz için e-posta adresinize ihtiyacımız var.</p>
-                    </div>
+            <Row className="forgotpass forgotpass--display forgotpass--margin-top">
+                <Col xs={12} md={4} >
+                    <h3 className="forgotpass-title">Şifre yenileme</h3>
+                    <p className="forgotpass-subtitle">Şifre yenileme bağlantısını gönderebilmemiz için e-posta adresinize ihtiyacımız var.</p>
                 </Col>
             </Row >
-            <Row className="justify-content-center">
-                <Col xs={4} >
+            <Row className="forgotpass forgotpass--display">
+                <Col xs={12} md={4} >
                     <div>
                         <form onSubmit={(e) => {
                             e.preventDefault();
@@ -27,9 +27,7 @@ const ForgotPassword = () => {
                             <FormGroup labelFor="text-emailforgot">
                                 <InputGroup type="email" id="text-emailforgot" placeholder="E-posta Adresi" />
                             </FormGroup>
-                            <div className="buttons">
-                                <Button className="buttons-btn bp3-button bp3-intent-success bp3-large bp3-fill" type="submit">Şifremi sıfırla</Button>
-                            </div>
+                            <Button className="buttons-btn bp3-button bp3-intent-success bp3-large bp3-fill" type="submit">Şifremi sıfırla</Button>
                         </form>
                     </div>
                 </Col>
