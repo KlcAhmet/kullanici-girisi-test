4e7d8ae3bfcbf974a512f7b4df5e0c01f9e68e64
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-02 17:30:04 +0300

    Navigation.js css import edildi

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom"
 /* Components */
 import { Col } from 'react-bootstrap'
 import { Button, Navbar, Alignment } from "@blueprintjs/core";
+/* Css */
+import '../css/Navigation.css'
 
 const Navigation = (() => {
     const [navLinks, setNavLinks] = useState([])
@@ -25,17 +27,17 @@ const Navigation = (() => {
             }]
         const resultNav = []
         link.forEach(({ title, link }, i) => {
-            resultNav.push(<Button className="bp3-button bp3-minimal" key={i}><NavLink exact to={link}>{title}</NavLink></Button>)
+            resultNav.push(<Button className="navBar-group-btn bp3-button bp3-minimal" key={i}><NavLink exact to={link}>{title}</NavLink></Button>)
         });
         setNavLinks(resultNav)
     }, [])
 
     return (
-        <Col xs={12} /* className="col--margin" */>
-            <Navbar className="bp3-dark">
-                <Navbar.Group align={Alignment.RIGHT}>
+        <Col xs={12} className="navBar">
+            <Navbar className="navBar-frame bp3-dark">
+                <Navbar.Group className="navBar-group" align={Alignment.RIGHT}>
                     {navLinks}
-                    <Button className="bp3-button bp3-minimal bp3-icon-cog" />
+                    <Button className="navBar-group-btn-cog bp3-button bp3-minimal bp3-icon-cog" />
                 </Navbar.Group>
             </Navbar>
         </Col>
