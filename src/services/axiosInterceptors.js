0b1506a3038a8aae9e82542b75ef3ef86632dc16
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-05 15:59:42 +0300

    message, event ile değiştirildi

--- src/services/axiosInterceptors.js
+++ src/services/axiosInterceptors.js
@@ -1,24 +1,22 @@
 import axios from "axios"
-import { Message } from '../component map/ComponentMap'
 import { Events } from '../utils/Eventbus/EventBusEvents'
 
 axios.interceptors.response.use(function (response) {
-    if (response.status === 200 && response.data.IsSuccess === false) Events("login", "unsuccessful", `${response.data.ResultType}, ${response.data.Result}`)
+    if (response.status === 200 && response.data.IsSuccess === false) Events("login", "unsuccessful", response.data.ResultType, response.data.Result)
     else if (response.status === 200 && response.data.IsSuccess === true) Events("login", "loginSuccess", response.data.Result.UserInfo.FirstName)
-    else if (response.status === 200 && response.data.IsSuccess === 1 && response.data.ResultType === "SUCCESS") Message("registersuccess", `${response.data.Result}`)
-
+    else if (response.status === 200 && response.data.IsSuccess === 1 && response.data.ResultType === "SUCCESS") Events("register", "registersuccess", `${response.data.Result}`)
 
     return response;
 }, function (err) {
     try {
-        if (err.response.status === 400) Message(0, "Bad Request", `${err.response.status}`,)
-        else if (err.response.status === 401) Message("UNAUTHORIZED", `${err.response.data.ResultType}`, `${err.response.data.Result}`)
-        else if (err.response.status === 500) Message(0, "Server Error", `${err.response.status}`)
+        if (err.response.status === 400) Events("error", "400", err.response.status)
+        else if (err.response.status === 401) Events("UNAUTHORIZED", "UNAUTHORIZED", err.response.data.ResultType, err.response.data.Result)
+        else if (err.response.status === 500) Events("error", "500", err.response.status)
 
         return Promise.reject(err);
     } catch (error) {
         if (!err.status) {
-            Message(0, "Network Error", "Büyük ihtimal şirkette internet gitti.")
+            Events("error", "Network Error", "Büyük ihtimal şirkette internet gitti.")
         }
         return Promise.reject(err)
     }
