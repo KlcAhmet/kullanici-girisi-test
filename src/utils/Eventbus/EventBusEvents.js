bef6e51d8a21f85e9fc1aa068d27a8bbdc03670b
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-05 15:02:32 +0300

    add unsuccessful event

--- src/utils/Eventbus/EventBusEvents.js
+++ src/utils/Eventbus/EventBusEvents.js
@@ -1,5 +1,7 @@
 import EventBus from './EventBus'
 
-export function Events(event, type, message) {
-    if (event === 'login' && type === 'loginSuccess') EventBus.emit("login", { type: "loginSuccess", message: { message } });
+export function Events(event, type, message, submessage) {
+    /* Login */
+    if (event === 'login' && type === 'loginSuccess') EventBus.emit("login", { type: "loginSuccess", message: message });
+    else if (event === 'login' && type === 'unsuccessful') EventBus.emit("login", { type: "unsuccessful", message: message });
 }
