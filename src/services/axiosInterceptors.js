ab757fa8f6e707d7f6e9b6d30c24af583148d931
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-05 06:00:35 +0300

    import eventbus

--- src/services/axiosInterceptors.js
+++ src/services/axiosInterceptors.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 import { Message } from '../component map/ComponentMap'
+import { Events } from '../utils/Eventbus/EventBusEvents'
 
 axios.interceptors.response.use(function (response) {
     if (response.status === 200 && response.data.IsSuccess === false) Message("unsuccessful", `${response.data.ResultType}, ${response.data.Result}`)
-    else if (response.status === 200 && response.data.IsSuccess === true) Message("loginSuccess", `${response.data.Result.UserInfo.FirstName}`)
+    else if (response.status === 200 && response.data.IsSuccess === true) {
+        Message("loginSuccess", `${response.data.Result.UserInfo.FirstName}`)
+        Events("login", "success", `${response.data.Result.UserInfo.FirstName}`)
+    }
     else if (response.status === 200 && response.data.IsSuccess === 1 && response.data.ResultType === "SUCCESS") Message("registersuccess", `${response.data.Result}`)
-    /* 
-    /Account/Create response dönmüyor
-    /Account/ForgotPassword sorun var
-    */
-    console.log(response.data);
+
 
     return response;
 }, function (err) {
@@ -17,11 +17,7 @@ axios.interceptors.response.use(function (response) {
         if (err.response.status === 400) Message(0, "Bad Request", `${err.response.status}`,)
         else if (err.response.status === 401) Message("UNAUTHORIZED", `${err.response.data.ResultType}`, `${err.response.data.Result}`)
         else if (err.response.status === 500) Message(0, "Server Error", `${err.response.status}`)
-        /* 
-         /Account/Create response dönmüyor
-         /Account/ForgotPassword sorun var
-        */
-        console.log(err.response);
+
         return Promise.reject(err);
     } catch (error) {
         if (!err.status) {
