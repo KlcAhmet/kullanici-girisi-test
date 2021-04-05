84d83a7ccb407e648837207cd01c7c4bbd311d4d
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-05 15:30:52 +0300

    add loading event

--- src/utils/Eventbus/EventBusEvents.js
+++ src/utils/Eventbus/EventBusEvents.js
@@ -4,4 +4,6 @@ export function Events(event, type, message, submessage) {
     /* Login */
     if (event === 'login' && type === 'loginSuccess') EventBus.emit("login", { type: "loginSuccess", message: message });
     else if (event === 'login' && type === 'unsuccessful') EventBus.emit("login", { type: "unsuccessful", message: message });
+    /* Loading */
+    else if (event === 'loading' && type === 'loading') EventBus.emit("loading", { type: "loading", message: message });
 }
