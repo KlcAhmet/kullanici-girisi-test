09242ee81f7c0e75fee0371272de0e69bc61fe54
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-10 02:47:08 +0300

    Message.js consts eklendi

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,16 +2,17 @@ import toastr from "toastr"
 /* Utils */
 import history from "../utils/history"
 import EventBus from "../utils/Eventbus/EventBus"
+import consts from "../consts.js"
 
 const Message = () => {
     toastr.options.escapeHtml = true;
     /* Response 200 */
-    EventBus.addListener("login", ({ type, message, submessage }) => {
-        if (type === 'loginSuccess') {
+    EventBus.addListener(consts.events.login, ({ type, message, submessage }) => {
+        if (type === consts.events.types.loginSuccess) {
             toastr.clear()
             toastr.success(`Merhaba ${message}. Giriş Başarılı.`)
         }
-        else if (type === 'unsuccessful') {
+        else if (type === consts.events.types.unsuccessful) {
             toastr.clear()
             toastr.warning(message, submessage)
             localStorage.removeItem('Token')
@@ -19,8 +20,8 @@ const Message = () => {
             localStorage.removeItem('ContactList')
         }
     })
-    EventBus.addListener("loading", ({ type, message, submessage }) => {
-        if (type === 'loading') {
+    EventBus.addListener(consts.events.loading, ({ type, message, submessage }) => {
+        if (type === consts.events.loading) {
             toastr.info(message, null, {
                 "closeButton": false,
                 "debug": false,
@@ -40,8 +41,8 @@ const Message = () => {
             })
         }
     })
-    EventBus.addListener("register", ({ type, message, submessage }) => {
-        if (type === 'registersuccess') {
+    EventBus.addListener(consts.events.register, ({ type, message, submessage }) => {
+        if (type === consts.events.types.registerSuccess) {
             toastr.clear()
             toastr.success(message, "Mail adresinizden üyeliğinizi onaylamayı unutmayın. Giriş sayfasına yönlendiriliyorsunuz.", {
                 "closeButton": false,
@@ -67,8 +68,8 @@ const Message = () => {
     })
 
     /* Response Error */
-    EventBus.addListener("UNAUTHORIZED", ({ type, message, submessage }) => {
-        if (type === 'UNAUTHORIZED') {
+    EventBus.addListener(consts.events.unauthorized, ({ type, message, submessage }) => {
+        if (type === consts.events.types.unauthorized) {
             toastr.clear()
             toastr.warning(message, `${submessage} Oturum açmaya yönlendiriliyorsunuz`, {
                 "closeButton": true,
@@ -95,7 +96,7 @@ const Message = () => {
             }, 4000);
         }
     })
-    EventBus.addListener("error", ({ type, message, submessage }) => {
+    EventBus.addListener(consts.events.error, ({ type, message, submessage }) => {
         if (type === '400') {
             toastr.warning("Hata kodu: 400", message)
         }
