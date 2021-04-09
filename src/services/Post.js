235dbf60c8ce22afa890f91141fa8216c8fa3d29
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-10 02:50:46 +0300

    Post.js consts eklendi

--- src/services/Post.js
+++ src/services/Post.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 import './axiosInterceptors'
 import Access from "../components/Access"
+/* Reduxs */
 import store from "../store/index"
 /* Utilst */
 import { Events } from '../utils/Eventbus/EventBusEvents'
+import consts from "../consts.js"
 
 // eslint-disable-next-line
 export default {
@@ -15,7 +17,7 @@ export default {
         var formdata = new FormData()
         formdata.append('Email', event.target[0].value)
         formdata.append('Password', event.target[1].value)
-        Events("loading", "loading", "Sunucudan haber bekleniyor")
+        Events(consts.events.login, consts.events.types.loading, "Sunucudan haber bekleniyor")
         return axios.post(`${Access}/Account/Login`, formdata, axiosParameters)
     },
     postContactList(event) {
@@ -34,32 +36,32 @@ export default {
 
         return axios.post(`${Access}/ContactUs/List`, formdata, axiosParameters)
     },
-    postAccountCreate(event) {
-        const axiosParameters = {
-            headers: { "Content-Type": "multipart/form-data" }
+    /*     postAccountCreate(event) {
+            const axiosParameters = {
+                headers: { "Content-Type": "multipart/form-data" }
+            }
+    
+            var formdata = new FormData()
+            formdata.append('Email', event.target[0].value)
+            formdata.append('Password', event.target[1].value)
+            formdata.append('FirstName', event.target[2].value)
+            formdata.append('LastName', event.target[3].value)
+            formdata.append('PhoneNumber', event.target[4].value)
+            formdata.append('BirthDate', event.target[5].value)
+            formdata.append('Gender', event.target[6].value)
+            formdata.append('Address', event.target[7].value)
+    
+            return axios.post(`${Access}/Account/Create`, formdata, axiosParameters)
+        },
+        forgotPassword(event) {
+            const axiosParameters = {
+                headers: { "Content-Type": "multipart/form-data" }
+            }
+    
+            var formdata = new FormData()
+            formdata.append('Email', event.target[0].value)
+    
+            return axios.post(`${Access}/Account/ForgotPassword`, formdata, axiosParameters)
         }
-
-        var formdata = new FormData()
-        formdata.append('Email', event.target[0].value)
-        formdata.append('Password', event.target[1].value)
-        formdata.append('FirstName', event.target[2].value)
-        formdata.append('LastName', event.target[3].value)
-        formdata.append('PhoneNumber', event.target[4].value)
-        formdata.append('BirthDate', event.target[5].value)
-        formdata.append('Gender', event.target[6].value)
-        formdata.append('Address', event.target[7].value)
-
-        return axios.post(`${Access}/Account/Create`, formdata, axiosParameters)
-    },
-    forgotPassword(event) {
-        const axiosParameters = {
-            headers: { "Content-Type": "multipart/form-data" }
-        }
-
-        var formdata = new FormData()
-        formdata.append('Email', event.target[0].value)
-
-        return axios.post(`${Access}/Account/ForgotPassword`, formdata, axiosParameters)
-    }
-
+     */
 }
