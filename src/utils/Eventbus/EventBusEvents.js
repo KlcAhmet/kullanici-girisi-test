a0c510adff04c019acc36c23504279229ad21516
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-10 02:41:03 +0300

    EventBusEvents.js consts eklendi

--- src/utils/Eventbus/EventBusEvents.js
+++ src/utils/Eventbus/EventBusEvents.js
@@ -1,16 +1,16 @@
 import EventBus from './EventBus'
-
+import consts from '../../consts.js'
 export function Events(event, type, message, submessage) {
     // RESPONSE 200
     /* Login */
-    if (event === 'login' && type === 'loginSuccess') EventBus.emit("login", { type: "loginSuccess", message: message });
-    else if (event === 'login' && type === 'unsuccessful') EventBus.emit("login", { type: "unsuccessful", message: message });
+    if (event === consts.events.login && type === consts.events.types.loginSuccess) EventBus.emit(consts.events.login, { type: consts.events.types.loginSuccess, message: message });
+    else if (event === consts.events.login && type === consts.events.types.unsuccessful) EventBus.emit(consts.events.login, { type: consts.events.types.unsuccessful, message: message });
     /* Loading */
-    else if (event === 'loading' && type === 'loading') EventBus.emit("loading", { type: "loading", message: message });
+    else if (event === consts.events.loading && type === consts.events.types.loading) EventBus.emit(consts.events.loading, { type: consts.events.types.loading, message: message });
     /* Register */
-    else if (event === 'register' && type === 'registersuccess') EventBus.emit("register", { type: "registersuccess", message: message });
+    else if (event === consts.events.register && type === consts.events.types.registerSuccess) EventBus.emit(consts.events.register, { type: consts.events.types.registerSuccess, message: message });
 
     // RESPONSE ERROR
-    else if (event === 'error') EventBus.emit("error", { type: type, message: message });
-    else if (event === 'UNAUTHORIZED' && type === 'UNAUTHORIZED') EventBus.emit("UNAUTHORIZED", { type: "UNAUTHORIZED", message: message, submessage: submessage });
+    else if (event === consts.events.error) EventBus.emit(consts.events.error, { type: type, message: message });
+    else if (event === consts.events.unauthorized && type === consts.events.types.unauthorized) EventBus.emit(consts.events.unauthorized, { type: consts.events.types.unauthorized, message: message, submessage: submessage });
 }
