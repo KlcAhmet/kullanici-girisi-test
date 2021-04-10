18ab0451f672d100d8e43e9f2bb4758c880e6cdc
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-04-11 02:08:03 +0300

    import Contex && Provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {
   Router, Switch, Route
 } from "react-router-dom";
-
 /* Components */
 import { Home, Login, Navigation, ProtectedRoute, Register, ForgotPassword, Message } from "./component map/ComponentMap"
 /* utils */
 import history from "./utils/history";
-
+/* Contex */
+import { UContex, UserContex } from "./Contex/Contex"
 /* CSS */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "toastr/build/toastr.min.css"
@@ -15,21 +15,24 @@ import "@blueprintjs/core/lib/css/blueprint.css"
 import "@blueprintjs/icons/lib/css/blueprint-icons.css"
 import { useEffect } from "react";
 
+
 function App() {
   useEffect(() => { Message() })
 
   return (
     <div className="App">
-      <Router history={history}>
-        <Navigation />
-        <Switch>
-          <Route path="/forgotpassword" component={ForgotPassword} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <ProtectedRoute path='/contact' />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
+      <UContex.Provider value={UserContex}>
+        <Router history={history}>
+          <Navigation />
+          <Switch>
+            <Route path="/forgotpassword" component={ForgotPassword} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <ProtectedRoute path='/contact' />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </UContex.Provider>
     </div>
   );
 }
